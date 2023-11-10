e1a10e83517f54a965cd8fd5c6b4c542bd9bc9a7
Andrew Bastien <volundmush@gmail.com>
2023-11-09 19:52:54 -0500

    Initial commit.

--- athanor_backscroll/commands.py
+++ athanor_backscroll/commands.py
@@ -24,14 +24,23 @@ class CmdBackscroll(AthanorCommand):
             except ValueError:
                 self.msg("Invalid count.", options={"no_backscroll": True})
                 return
-        if not (backscroll := self.caller.backscroll_records.all()):
+        if not (
+            backscroll := self.caller.backscroll_records.all().order_by("-timestamp")[
+                :count
+            ]
+        ):
             self.msg("No backscroll found.", options={"no_backscroll": True})
             return
 
         out = []
-        for record in backscroll[-count:]:
+        for record in reversed(backscroll):
             out.append(
-                f"> {self.account.datetime_format(dt=record.timestamp, template='%c')}\n{record.text}"
+                f"> {self.account.datetime_format(dt=record.timestamp, template='%c')}\n{record.text}|n"
             )
 
-        self.msg("\n".join(backscroll[-count:]), options={"no_backscroll": True})
+        self.msg("\n".join(out), options={"no_backscroll": True})
+
+        self.msg(
+            f"(Displayed {len(out)} Lines of {self.caller.backscroll_records.count()})",
+            options={"no_backscroll": True},
+        )
--- athanor_backscroll/events.py
+++ athanor_backscroll/events.py
@@ -29,16 +29,16 @@ def rich_print(*args, **kwargs):
     return console.export_text(clear=True, styles=True)
 
 
-def character_at_post_msg_receive(character, from_obj=None, **kwargs):
+def character_at_post_msg_receive(sender, **kwargs):
     if options := kwargs.get("options", dict()):
         if options.get("no_backscroll", False):
             return
     if text_kw := kwargs.get("text", None):
         text = text_kw[0] if isinstance(text_kw, tuple) else text_kw
         if text:
-            character.backscroll_records.create(text=text)
+            sender.backscroll_records.create(text=text)
     if rich_kw := kwargs.get("rich", None):
         rich_renderable = rich_kw[0] if isinstance(rich_kw, tuple) else rich_kw
         text = rich_print(rich_renderable)
         if text:
-            character.backscroll_records.create(text=text)
+            sender.backscroll_records.create(text=text)
